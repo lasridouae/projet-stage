fdfc6c5681ee18756107bfb356e6dc8dba4bcb6e
lasridouae <lasridouae1@gmail.com>
2020-07-25 02:52:44 +0100

    .

--- ka-projet/_config/db.php
+++
@@ -1,3 +0,0 @@
-<?php 
-
-$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASS);
--- ka-projet/views/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-  <!-- ALL JS FILES -->
-  <script src="js/all.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- ALL PLUGINS -->
-    <script src="assets/contact/jqBootstrapValidation.js"></script>
-    <script src="assets/js/custom.js"></script>   
-</footer>
